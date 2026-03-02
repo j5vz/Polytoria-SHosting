0a8bea7cab82656983cfe71c83c28338ddacb7e8
Nexus <108929448+j5vz@users.noreply.github.com>
2026-03-03 01:21:01 +0300

    Update launcher.py

--- launcher.py
+++ launcher.py
@@ -1,21 +1,24 @@
 import subprocess
 import os
 import sys
+import ctypes
 
 def main():
-    base = os.path.dirname(os.path.abspath(sys.argv[0]))
+    if os.name == 'nt':
+        ctypes.windll.kernel32.SetConsoleTitleW("SHoster - V1.1")
     
-    # finds the first .poly file in the folder automatically
+    base = os.path.dirname(os.path.abspath(sys.argv[0]))
     map_file = next((f for f in os.listdir(base) if f.endswith('.poly')), None)
-    # searches for the client (checks common names cuz im lazy)
     client = next((f for f in os.listdir(base) if f.lower() in ["polytoria client.exe", "polytoria.exe"]), None)
 
     if not client:
         print("[-] Put this in the folder with your Polytoria exe.")
-        input(); return
+        input()
+        return
     if not map_file:
         print("[-] No .poly / Polytoria game file found in this folder!")
-        input(); return
+        input()
+        return
 
     print(f"[+] Launching {map_file}...")
     subprocess.Popen([os.path.join(base, client), "-solo", os.path.join(base, map_file), "-randomuser"])
